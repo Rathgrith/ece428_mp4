7df752dd06b9355b5050769e8ee49d93426a6e95
dl58 <dl58@illinois.edu>
2023-11-23 11:29:19 -0600

    rpc-ify the calls to MapleJuice task

---
+++ cmd/Server/MapleJuiceServer.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"os/exec"
+
+	"google.golang.org/grpc"
+
+	"ece428_mp4/idl" // Adjust with the actual generated package path
+)
+
+type server struct {
+	idl.UnimplementedMapleJuiceServiceServer
+}
+
+func (s *server) ExecuteMaple(ctx context.Context, req *idl.MapleRequest) (*idl.MapleResponse, error) {
+	output, err := runCommand(req.Exe, "-input", req.SrcDir, "-prefix", req.Prefix, "-regex", "Anthony")
+	if err != nil {
+		return nil, err
+	}
+	return &idl.MapleResponse{Output: output}, nil
+}
+
+func (s *server) ExecuteJuice(ctx context.Context, req *idl.JuiceRequest) (*idl.JuiceResponse, error) {
+	fmt.Printf("Received Juice request: %+v\n", req)
+	output, err := runCommand(req.Exe, "-key", req.Key, "-prefix", req.Prefix, "-output", req.OutDir)
+	if err != nil {
+		return nil, err
+	}
+	return &idl.JuiceResponse{Output: output}, nil
+}
+
+func runCommand(name string, args ...string) (string, error) {
+	cmd := exec.Command(name, args...)
+	out, err := cmd.CombinedOutput()
+	return string(out), err
+}
+
+func main() {
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	idl.RegisterMapleJuiceServiceServer(s, &server{})
+	log.Printf("server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
--- go.mod
+++ go.mod
@@ -3,9 +3,15 @@ module ece428_mp4
 go 1.21.1
 
 require (
+	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/mattn/go-runewidth v0.0.15 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/theckman/go-flock v0.8.1 // indirect
+	golang.org/x/net v0.14.0 // indirect
 	golang.org/x/sys v0.14.0 // indirect
+	golang.org/x/text v0.12.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20230822172742-b8732ec3820d // indirect
+	google.golang.org/grpc v1.59.0 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
 	gopkg.in/cheggaaa/pb.v1 v1.0.28 // indirect
 )
