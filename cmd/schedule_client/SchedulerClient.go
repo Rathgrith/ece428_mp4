ad4772b2d248e97c027e0edaa5b87c7b858c7234
feize <feizes2@illinois.edu>
2023-12-01 16:00:43 -0600

    simple merge

---
+++ cmd/schedule_client/SchedulerClient.go
@@ -0,0 +1,164 @@
+package main
+
+import (
+	"context"
+	"ece428_mp4/idl"
+	"errors"
+	"flag"
+	"fmt"
+	"log"
+	"strconv"
+	"strings"
+	"time"
+
+	"google.golang.org/grpc"
+)
+
+type SQLQuery struct {
+	Operation   string
+	Datasets    []string
+	Condition   string
+	JoinColumn1 string
+	JoinColumn2 string
+}
+
+func parseSQLQuery(sql string) (SQLQuery, error) {
+	parts := strings.Fields(strings.ToLower(sql))
+	// fmt.Println(parts[3])
+	// fmt.Println(parts[5])
+	if len(parts) < 6 {
+		return SQLQuery{}, errors.New("invalid query format")
+	}
+
+	query := SQLQuery{}
+	if parts[2] == "from" && parts[4] == "where" {
+		// Filter operation
+		query.Operation = "filter"
+		query.Datasets = []string{parts[3]}
+		query.Condition = strings.Join(parts[5:], " ")
+	} else if len(parts) >= 7 && strings.Contains(parts[3], ",") && parts[5] == "where" {
+		// Join operation
+		query.Operation = "join"
+		dataset1 := strings.Split(parts[3], ",")[0]
+		query.Datasets = []string{dataset1, parts[4]}
+		query.JoinColumn1 = strings.Split(parts[6], ".")[1]
+		query.JoinColumn2 = strings.Split(parts[8], ".")[1]
+	} else {
+		return SQLQuery{}, errors.New("unsupported query type")
+	}
+
+	return query, nil
+}
+
+func main() {
+	serverAddr := flag.String("server_addr", "fa23-cs425-4801.cs.illinois.edu:50051", "The server address in the format of host:port")
+	// taskType := flag.String("type", "maple", "Type of the task (maple or juice)")
+	// executable := flag.String("exe", "filtermaple", "Path to the executable")
+	numJobs := flag.Int("numJobs", 1, "Number of maple tasks")
+	prefix := flag.String("prefix", "inter", "Prefix for intermediate files")
+	srcDir1 := flag.String("src_dir1", "./test1.csv", "Source directory 1 for maple/juice task")
+	srcDir2 := flag.String("src_dir2", "", "Source directory 2 for juice task (optional)")
+	regex := flag.String("regex", "Bloom", "Regex pattern for filter (optional)")
+	joinColumn1 := flag.String("join_col1", "-1", "Join column for the first dataset (optional)")
+	joinColumn2 := flag.String("join_col2", "-1", "Join column for the second dataset (optional)")
+	outDir := flag.String("out_dir", "output.csv", "Output directory for juice task")
+	sqlQuery := flag.String("sql", "", "SQL-like query for filter operation")
+
+	flag.Parse()
+
+	// Parse the SQL query if provided
+	var query SQLQuery
+	var err error
+	if *sqlQuery != "" {
+		query, err = parseSQLQuery(*sqlQuery)
+		if err != nil {
+			//log.Fatalf("Invalid SQL query: %v", err)
+		}
+		if query.Operation == "filter" {
+			*srcDir1 = query.Datasets[0]
+			*regex = query.Condition
+		}
+		if query.Operation == "join" {
+			*srcDir1 = query.Datasets[0]
+			*srcDir2 = query.Datasets[1]
+			*joinColumn1 = query.JoinColumn1
+			*joinColumn2 = query.JoinColumn2
+		}
+	}
+
+	// Convert join column strings to integers
+	joinCol1, err := strconv.Atoi(*joinColumn1)
+	if err != nil {
+		joinCol1 = -1
+	}
+	joinCol2, err := strconv.Atoi(*joinColumn2)
+	if err != nil {
+		joinCol2 = -1
+	}
+	// Set up a connection to the server
+	conn, err := grpc.Dial(*serverAddr, grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	client := idl.NewMapleJuiceSchedulerClient(conn)
+
+	// Contact the server and print out its response
+	// ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+	defer cancel()
+	query.Operation = "filter"
+	if query.Operation == "filter" {
+		mapleResp, err := client.EnqueueTask(ctx, &idl.TaskRequest{
+			TaskType: "maple",
+			Exe:      "./filterMaple.exe",
+			NumJobs:  int32(*numJobs),
+			Prefix:   *prefix,
+			SrcDir1:  *srcDir1,
+			Regex:    *regex,
+			DestFile: *outDir,
+		})
+		if err != nil {
+			log.Fatalf("could not enqueue task: %v", err)
+		}
+		fmt.Printf("Server response: %s\n", mapleResp.GetMessage())
+		juiceResp, err := client.EnqueueTask(ctx, &idl.TaskRequest{
+			TaskType: "juice",
+			Exe:      "./filterJuice.exe",
+			NumJobs:  int32(*numJobs),
+			Prefix:   *prefix,
+			DestFile: *outDir,
+		})
+		if err != nil {
+			log.Fatalf("could not enqueue task: %v", err)
+		}
+		fmt.Printf("Server response: %s\n", juiceResp.GetMessage())
+	} else if query.Operation == "join" {
+		joinmResp, err := client.EnqueueTask(ctx, &idl.TaskRequest{
+			TaskType:    "maple",
+			Exe:         "./joinMaple.exe",
+			NumJobs:     int32(*numJobs),
+			SrcDir1:     *srcDir1,
+			SrcDir2:     *srcDir2,
+			Prefix:      *prefix,
+			JoinColumn1: int32(joinCol1),
+			JoinColumn2: int32(joinCol2),
+			DestFile:    *outDir,
+		})
+		if err != nil {
+			log.Fatalf("could not enqueue task: %v", err)
+		}
+		fmt.Printf("Server response: %s\n", joinmResp.GetMessage())
+		joinjResp, err := client.EnqueueTask(ctx, &idl.TaskRequest{
+			TaskType: "juice",
+			Exe:      "./joinJuice.exe",
+			NumJobs:  int32(*numJobs),
+			Prefix:   *prefix,
+			DestFile: *outDir,
+		})
+		if err != nil {
+			log.Fatalf("could not enqueue task: %v", err)
+		}
+		fmt.Printf("Server response: %s\n", joinjResp.GetMessage())
+	}
+}
---
+++ cmd/schedule_server/Scheduler.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"context"
+	"ece428_mp4/idl"
+	"ece428_mp4/pkg/logutil"
+	"ece428_mp4/pkg/maple_juice/job"
+	"fmt"
+	"github.com/sirupsen/logrus"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+type server struct {
+	idl.UnimplementedMapleJuiceSchedulerServer
+	taskQueue  chan Task // Channel used as a FIFO queue
+	jobManager *job.Manager
+}
+
+func (s *server) EnqueueTask(ctx context.Context, req *idl.TaskRequest) (*idl.TaskResponse, error) {
+	fmt.Printf("Received task request: %+v\n", req)
+	fmt.Printf("Task queue length: %d\n", len(s.taskQueue))
+	// Create a channel for task completion signal
+	completion := make(chan string, 1)
+	task := Task{
+		Type:          req.TaskType,
+		Executable:    req.Exe,
+		NumTasks:      int(req.NumJobs),
+		Prefix:        req.Prefix,
+		SrcDir1:       req.SrcDir1,
+		SrcDir2:       req.SrcDir2,
+		Regex:         req.Regex,
+		JoinColumn1:   req.JoinColumn1,
+		JoinColumn2:   req.JoinColumn2,
+		OutDir:        req.DestFile,
+		completionSig: completion,
+	}
+
+	// Enqueue the task
+	s.taskQueue <- task
+	result := <-completion
+	close(completion)
+
+	return &idl.TaskResponse{Message: result}, nil
+	// return &idl.TaskResponse{Message: "Task enqueued successfully"}, nil
+}
+
+// Task represents a MapleJuice task
+type Task struct {
+	Type          string // "maple" or "juice"
+	Executable    string
+	NumTasks      int
+	Prefix        string
+	SrcDir1       string
+	SrcDir2       string
+	Regex         string
+	JoinColumn1   int32
+	JoinColumn2   int32
+	OutDir        string
+	completionSig chan string // Channel to signal completion
+}
+
+// executeTask simulates task execution
+func executeTask(jobManager *job.Manager, task Task) {
+	fmt.Printf("Executing task: %+v\n", task)
+
+	err := jobManager.SubmitMapleJob(&idl.ExecuteMapleJobRequest{
+		ExeName:                    task.Executable,
+		IntermediateFilenamePrefix: task.Prefix,
+		InputFiles:                 []string{task.SrcDir1},
+		NumMaples:                  int32(task.NumTasks),
+		ExeArgs:                    []string{fmt.Sprintf("-regex %s", task.Regex)},
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Task completed: %+v\n", task)
+	// Signal task completion
+	task.completionSig <- "Task completed successfully with output file: " + task.OutDir
+}
+
+func main() {
+	err := logutil.InitDefaultLogger(logrus.DebugLevel)
+	if err != nil {
+		panic(err)
+	}
+	taskQueue := make(chan Task, 100) // Task queue with a buffer of 100 tasks
+
+	// Create and start the gRPC server
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	grpcServer := grpc.NewServer()
+	sqlServer := &server{taskQueue: taskQueue, jobManager: job.NewJobManager()}
+	idl.RegisterMapleJuiceSchedulerServer(grpcServer, sqlServer)
+
+	// Start the scheduler in a separate goroutine
+	go func() {
+		for task := range taskQueue {
+			executeTask(sqlServer.jobManager, task)
+		}
+	}()
+
+	log.Println("Starting gRPC server on port 50051")
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
