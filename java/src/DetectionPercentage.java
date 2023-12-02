b3181f0f9489d2f56b47a689596de79f6e6dead1
dl58 <dl58@illinois.edu>
2023-12-02 12:59:27 -0600

    hadoop_demo

--- java/src/DetectionPercentage.java
+++ java/src/DetectionPercentage.java
@@ -26,9 +26,9 @@ public class DetectionPercentage {
 
         public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
             String[] dataArray = value.toString().split(","); // split the data into array
-            if (dataArray.length > 10) { // avoid null pointer exception
-                if (dataArray[10].trim().equals(interconneType)) { // check interconne type at index 10
-                    word.set(dataArray[9]); // set 'Detection_' value from index 9
+            if (dataArray.length > 1) { // avoid null pointer exception
+                if (dataArray[0].trim().equals(interconneType)) { // check interconne type
+                    word.set(dataArray[1]); // set 'Detection_' value
                     context.write(word, one);
                 }
             }
@@ -59,14 +59,15 @@ public class DetectionPercentage {
 
     public static void main(String[] args) throws Exception {
         Configuration conf = new Configuration();
-        conf.set("interconneType", args[2]);
+        conf.set("interconneType", args[2]); // set interconneType from command line argument
+
         Job job = Job.getInstance(conf, "detection percentage");
         job.setJarByClass(DetectionPercentage.class);
         job.setMapperClass(TokenizerMapper.class);
         job.setCombinerClass(IntSumReducer.class);
         job.setReducerClass(IntSumReducer.class);
         job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(Text.class); // Set output value class to Text
+        job.setOutputValueClass(IntWritable.class);
         FileInputFormat.addInputPath(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
 
