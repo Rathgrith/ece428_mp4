ad50e480741192818c21fd93f2eb64a28c4e85b1
dl58 <dl58@illinois.edu>
2023-12-02 13:26:34 -0600

    scheduler_adjust

--- java/src/DetectionPercentage.java
+++ java/src/DetectionPercentage.java
@@ -1,73 +1,65 @@
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.FloatWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
-public class DetectionPercentage {
-
-    public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable> {
-
-        private final static IntWritable one = new IntWritable(1);
-        private Text word = new Text();
-        private String interconneType;
-
-        @Override
-        protected void setup(Context context) throws IOException, InterruptedException {
-            interconneType = context.getConfiguration().get("interconneType");
-        }
+public class TrafficSignalComposition {
+    public static class TrafficSignalMapper extends Mapper<Object, Text, Text, FloatWritable> {
+        private final Text detectionType = new Text();
+        private final FloatWritable one = new FloatWritable(1);
 
         public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-            String[] dataArray = value.toString().split(","); // split the data into array
-            if (dataArray.length > 10) { // avoid null pointer exception
-                if (dataArray[10].trim().equals(interconneType)) { // check interconne type
-                    word.set(dataArray[9]); // set 'Detection_' value
-                    context.write(word, one);
+            String[] columns = value.toString().split(",");
+            if (columns.length >= 2) {
+                String interconne = columns[0].trim();
+                String detection = columns[1].trim();
+
+                // Check if the 'Interconne' type matches the provided parameter 'X'
+                if (interconne.equals(context.getConfiguration().get("interconneType"))) {
+                    detectionType.set(detection);
+                    context.write(detectionType, one);
                 }
             }
         }
     }
 
-    public static class IntSumReducer extends Reducer<Text, IntWritable, Text, Text> {
+    public static class TrafficSignalReducer extends Reducer<Text, FloatWritable, Text, FloatWritable> {
+        private final FloatWritable result = new FloatWritable();
+        private float totalCount = 0;
 
-        private Map<String, Integer> countMap = new HashMap<>();
-
-        public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
-            int sum = 0;
-            for (IntWritable val : values) {
+        public void reduce(Text key, Iterable<FloatWritable> values, Context context)
+                throws IOException, InterruptedException {
+            float sum = 0;
+            for (FloatWritable val : values) {
                 sum += val.get();
+                totalCount += val.get();
             }
-            countMap.put(key.toString(), sum);
-        }
-
-        @Override
-        protected void cleanup(Context context) throws IOException, InterruptedException {
-            int totalCount = countMap.values().stream().mapToInt(Integer::intValue).sum();
-            for (Map.Entry<String, Integer> entry : countMap.entrySet()) {
-                double percentage = 100.0 * entry.getValue() / totalCount;
-                context.write(new Text(entry.getKey()), new Text(percentage + "%"));
-            }
+            result.set((sum / totalCount) * 100); // Calculate the percentage composition
+            context.write(key, result);
         }
     }
 
     public static void main(String[] args) throws Exception {
         Configuration conf = new Configuration();
-        conf.set("interconneType", args[2]); // set interconneType from command line argument
+        conf.set("interconneType", args[2]); // Set the 'Interconne' type as a configuration parameter
 
-        Job job = Job.getInstance(conf, "detection percentage");
-        job.setJarByClass(DetectionPercentage.class);
-        job.setMapperClass(TokenizerMapper.class);
-        job.setCombinerClass(IntSumReducer.class);
-        job.setReducerClass(IntSumReducer.class);
+        Job job = Job.getInstance(conf, "Traffic Signal Composition");
+        job.setJarByClass(TrafficSignalComposition.class);
+        job.setMapperClass(TrafficSignalMapper.class);
+        job.setCombinerClass(TrafficSignalReducer.class);
+        job.setReducerClass(TrafficSignalReducer.class);
         job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(IntWritable.class);
+        job.setOutputValueClass(FloatWritable.class);
+
         FileInputFormat.addInputPath(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
 
