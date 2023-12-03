66da04518fdd4dcdec5c05cf4ed7893c7fd3250d
dl58 <dl58@illinois.edu>
2023-12-02 20:37:33 -0600

    scheduler_adjust

---
+++ java/src/RegexFilteredLines.java
@@ -0,0 +1,67 @@
+import java.io.IOException;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+import org.apache.hadoop.io.FloatWritable;
+import org.apache.hadoop.mapreduce.lib.input.NLineInputFormat;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class RegexFilteredLines {
+    public static class RegexMapper extends Mapper<Object, Text, Text, Text> {
+        private Pattern pattern;
+        private Text outKey = new Text();
+        private Text outValue = new Text();
+
+        @Override
+        protected void setup(Context context) throws IOException, InterruptedException {
+            Configuration conf = context.getConfiguration();
+            String regex = conf.get("filter.regex");
+            pattern = Pattern.compile(regex);
+        }
+
+        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+            Matcher matcher = pattern.matcher(value.toString());
+            if (matcher.find()) {
+                outKey.set(matcher.group());
+                outValue.set(value);
+                context.write(outKey, outValue);
+            }
+        }
+    }
+
+    public static class SimpleReducer extends Reducer<Text, Text, Text, Text> {
+        private Text result = new Text();
+
+        public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+            for (Text val : values) {
+                result.set(val);
+                context.write(key, result);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        conf.set("filter.regex", args[2]); // Set the regex as a configuration parameter
+
+        Job job = Job.getInstance(conf, "Regex Filtered Lines");
+        job.setJarByClass(RegexFilteredLines.class);
+        job.setMapperClass(RegexMapper.class);
+        job.setReducerClass(SimpleReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(Text.class);
+        job.setInputFormatClass(NLineInputFormat.class);
+        NLineInputFormat.setNumLinesPerSplit(job, 100);
+
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+}
